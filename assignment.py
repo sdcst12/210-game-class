199900fbff9ce094b57a634324e49e45426163e9
hungrybeagle <cyang@deltalearns.ca>
2023-10-11 12:09:53 -0700

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,3 +1,11 @@
 #! python3
 
-# SD Computing Studies Assignment
+class game:
+
+
+  def __init__(self):
+    pass
+
+
+# This is the only command allowed that is not in the class template. All code must be done there.
+g = game()
